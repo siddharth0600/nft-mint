737262a061dfe134e42c43f423c28736dc791850
siddharth0600 <siddharthtripathi00@gmail.com>
2022-04-14 16:35:30 +0530

    Collection card half done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
+import CollectionCard from "./components/CollectionCard";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg"
+      />
     </div>
   );
 }
