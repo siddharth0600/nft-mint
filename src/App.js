bd9bfe91425866a4b93993952f08bb726e5f6c37
siddharth0600 <siddharthtripathi00@gmail.com>
2022-04-22 16:47:05 +0530

    PunkList complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import CollectionCard from "./components/CollectionCard";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Punklist from "./components/Punklist";
 
 function App() {
+  const [punkListData, setPunkListData] = useState([]);
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xA6e26c8869587bD881873dE73e4937072Ea5Fa49&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <CollectionCard
-        id={0}
-        name={"Bandana Punk"}
-        traits={[{ value: 7 }]}
-        image="https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg"
-      />
+
+      <Punklist punkListData={punkListData} />
     </div>
   );
 }
